7af9674752c3e55c55605a2c5e75975dbc8ccc1b
Mingming Chen <mzc796@psu.edu>
2024-07-18 17:49:34 -0400

    add note.txt

--- bash/req_topo_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "request topo $1"
-curl -s -u admin:admin -X GET "http://$IP_ADDR:8181/restconf/operational/network-topology:network-topology/topology/flow:1" -H "content-type: application/json"
